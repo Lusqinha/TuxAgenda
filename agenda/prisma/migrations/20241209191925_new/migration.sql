208afbc8596e77f18b2d3fd268fc81c00414040b
Lucas Borges <contato@devlucasborges.com>
2024-12-10 01:45:41 -0300

    Editando models e removendo migracoes

--- agenda/prisma/migrations/20241209191925_new/migration.sql
+++ agenda/prisma/migrations/20241209191925_new/migration.sql
@@ -1,13 +1,9 @@
--- CreateEnum
-CREATE TYPE "Status" AS ENUM ('PENDING', 'CONFIRMED', 'CANCELLED');
-
 -- CreateTable
 CREATE TABLE "Service" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "description" TEXT,
+    "minutes_duration" INTEGER NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
-    "duration" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -17,22 +13,17 @@ CREATE TABLE "Service" (
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" SERIAL NOT NULL,
-    "fullName" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "cpf" TEXT NOT NULL,
-    "phone" TEXT,
-    "email" TEXT,
-    "serviceId" INTEGER NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "status" "Status" NOT NULL DEFAULT 'PENDING',
+    "phone" TEXT NOT NULL,
     "notes" TEXT,
+    "date" TIMESTAMP(3) NOT NULL,
+    "time" TEXT NOT NULL,
+    "serviceId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Appointment_cpf_date_key" ON "Appointment"("cpf", "date");
 
 -- AddForeignKey
 ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "Service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
