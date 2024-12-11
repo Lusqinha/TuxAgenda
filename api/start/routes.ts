792fef329a9688173e4ec9a4d327fcafa555ed08
Lucas Borges <contato@devlucasborges.com>
2024-12-11 03:06:47 -0300

    API: Rotas bases criadas e expostas

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -1,16 +1,19 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+import AppointmentReservedsController from '#controllers/appointment_reserveds_controller'
+import AppointmentsController from '#controllers/appointments_controller'
+import ColaboratorsController from '#controllers/colaborators_controller'
+import ServicesController from '#controllers/services_controller'
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.group(() => {
+  
+  router.resource('appointment_reserveds', AppointmentReservedsController).apiOnly()
+
+  router.resource('appointments', AppointmentsController).apiOnly()
+
+  router.resource('colaborators', ColaboratorsController).apiOnly()
+
+  router.resource('services', ServicesController).apiOnly()
+
+}).prefix('/api/v1')
+
+
